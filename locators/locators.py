b7a95ca2141d238900015f84acaba5d7e371c000
almas.hossain <almas.hossain@reddotdigitalit.com>
2025-03-17 10:17:08 +0600

    pytest ci added!

--- locators/locators.py
+++ locators/locators.py
@@ -7,8 +7,10 @@ class Locators:
     def locator(self):
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         _file_path = str(os.path.abspath(cur_dir))[:-8]
+        _file_path = os.path.abspath(cur_dir)
+        _file = os.path.join(_file_path, "locators.ini")
         # _file = _file_path + "\\" + "locators.ini"
-        _file = _file_path + "/" + "locators.ini"
+        # _file = _file_path + "/" + "locators.ini"
         parser = configparser.ConfigParser()
         parser.read(_file)
         return parser
