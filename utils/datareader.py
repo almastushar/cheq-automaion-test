16357995c833aecd5807fda3914b00badbcfc308
almas.hossain <almas.hossain@reddotdigitalit.com>
2025-03-17 10:19:27 +0600

    pytest ci added!

--- utils/datareader.py
+++ utils/datareader.py
@@ -6,7 +6,9 @@ class Data:
     def data(self):
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         _file_path = str(os.path.abspath(cur_dir))[:-5]
-        _file = _file_path + "\\" + "data.ini"
+        # _file_path = os.path.abspath(cur_dir)
+        _file = os.path.join(_file_path, "data.ini")
+        # _file = _file_path + "\\" + "data.ini"
         print(_file_path)
         parser = configparser.ConfigParser()
         parser.read(_file)
